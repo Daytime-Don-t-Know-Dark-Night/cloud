15f8f95235fa8f0eaaa8cfa3d0f96110662faefd
dingc <1109928059@qq.com>
2021-12-13 21:57:32 +0800

    spring security 获取当前登录用户信息

--- yeb-admin/yeb-server/src/main/java/com/xxxx/server/mapper/AdminMapper.java
+++ yeb-admin/yeb-server/src/main/java/com/xxxx/server/mapper/AdminMapper.java
@@ -2,6 +2,7 @@ package com.xxxx.server.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xxxx.server.pojo.Admin;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.xxxx.server.pojo.Admin;
  * @author dingc
  * @since 2021-12-12
  */
+// @Repository, 加上之后, impl层不会再报错
 public interface AdminMapper extends BaseMapper<Admin> {
 
 }
--- yeb-admin/yeb-server/src/main/java/com/xxxx/server/service/IAdminService.java
+++ yeb-admin/yeb-server/src/main/java/com/xxxx/server/service/IAdminService.java
@@ -2,15 +2,36 @@ package com.xxxx.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xxxx.server.pojo.Admin;
+import com.xxxx.server.pojo.RespBean;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author dingc
  * @since 2021-12-12
  */
 public interface IAdminService extends IService<Admin> {
+
+	/**
+	 * 登录之后返回 token
+	 *
+	 * @param username
+	 * @param password
+	 * @param request
+	 * @return
+	 */
+	RespBean login(String username, String password, HttpServletRequest request);
+
+	/**
+	 * 根据用户名获取用户
+	 *
+	 * @param username
+	 * @return
+	 */
+	Admin getAdminByUserName(String username);
 
 }
