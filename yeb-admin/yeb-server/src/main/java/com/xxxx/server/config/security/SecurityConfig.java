2d976fa8f8ffac97ffa36dd2b9fe7f6941ef2cf9
dingc <1109928059@qq.com>
2021-12-15 22:00:59 +0800

    整合swagger

--- yeb-admin/yeb-server/src/main/java/com/xxxx/server/config/security/SecurityConfig.java
+++ yeb-admin/yeb-server/src/main/java/com/xxxx/server/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,6 +36,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// super.configure(web);
+		web.ignoring().antMatchers(
+				"/login",
+				"/logout",
+				"/css/**",
+				"/js/**",
+				"/index.html",
+				"favicon.ico",
+				"/doc.html",
+				"/webjars/**",
+				"/swagger-resources/**",
+				"/v2/api-docs/**"
+		);
 	}
 
 	@Override
@@ -84,7 +102,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Bean
-	public JwtAuthencationTokenFilter jwtAuthencationTokenFilter(){
+	public JwtAuthencationTokenFilter jwtAuthencationTokenFilter() {
 		return new JwtAuthencationTokenFilter();
 	}
 }
