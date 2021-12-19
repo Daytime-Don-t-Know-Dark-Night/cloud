e0348dc00852bb2d327b5df0d8152cccdcf29f7f
dingc <1109928059@qq.com>
2021-12-19 20:54:22 +0800

    判断用户角色

--- yeb-admin/yeb-server/src/main/java/com/xxxx/server/mapper/RoleMapper.java
+++ yeb-admin/yeb-server/src/main/java/com/xxxx/server/mapper/RoleMapper.java
@@ -3,9 +3,11 @@ package com.xxxx.server.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xxxx.server.pojo.Role;
 
+import java.util.List;
+
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author dingc
@@ -13,4 +15,11 @@ import com.xxxx.server.pojo.Role;
  */
 public interface RoleMapper extends BaseMapper<Role> {
 
+	/**
+	 * 根据用户id查询角色列表
+	 *
+	 * @param adminId
+	 * @return
+	 */
+	List<Role> getRoles(Integer adminId);
 }
--- yeb-admin/yeb-server/src/main/java/com/xxxx/server/service/IAdminService.java
+++ yeb-admin/yeb-server/src/main/java/com/xxxx/server/service/IAdminService.java
@@ -3,8 +3,10 @@ package com.xxxx.server.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xxxx.server.pojo.Admin;
 import com.xxxx.server.pojo.RespBean;
+import com.xxxx.server.pojo.Role;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * <p>
@@ -35,4 +37,11 @@ public interface IAdminService extends IService<Admin> {
 	 */
 	Admin getAdminByUserName(String username);
 
+	/**
+	 * 根据用户id查询角色列表
+	 *
+	 * @param adminId
+	 * @return
+	 */
+	List<Role> getRoles(Integer adminId);
 }
