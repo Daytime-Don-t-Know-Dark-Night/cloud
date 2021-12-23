3fdb021933f08d64b27dde459a456dc603d188a5
dingc <1109928059@qq.com>
2021-12-23 21:36:59 +0800

    注销

--- yeb/src/views/Home.vue
+++ yeb/src/views/Home.vue
@@ -3,14 +3,14 @@
 		<el-container>
 			<el-header class="homeHeader">
 				<div class="title">云E办</div>
-				<el-dropdown class="userInfo">
+				<el-dropdown class="userInfo" @command="commandHandler">
 					<span class="el-dropdown-link">
 					    {{ user.name }}<i><img :src="user.userFace"></i>
 					</span>
 					<el-dropdown-menu slot="dropdown">
-						<el-dropdown-item>个人中心</el-dropdown-item>
-						<el-dropdown-item>设置</el-dropdown-item>
-						<el-dropdown-item>注销登录</el-dropdown-item>
+						<el-dropdown-item command="userinfo">个人中心</el-dropdown-item>
+						<el-dropdown-item command="setting">设置</el-dropdown-item>
+						<el-dropdown-item command="logout">注销登录</el-dropdown-item>
 					</el-dropdown-menu>
 				</el-dropdown>
 			</el-header>
@@ -51,6 +51,32 @@ export default {
 	computed: {
 		routes() {
 			return this.$store.state.routes;
+		}
+	},
+	methods: {
+		commandHandler(command) {
+			if (command === 'logout') {
+				this.$confirm('此操作将注销登录, 是否继续?', '提示', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+					this.postRequest('/logout');
+					// 清空用户信息
+					window.sessionStorage.removeItem("tokenStr");
+					window.sessionStorage.removeItem("user");
+					// 清空菜单
+					this.$store.commit("initRouters", []);
+					// 重定向
+					this.$router.replace("/");
+				}).catch(() => {
+					this.$message({
+						type: 'info',
+						message: '操作已取消'
+					});
+				});
+
+			}
 		}
 	}
 }
