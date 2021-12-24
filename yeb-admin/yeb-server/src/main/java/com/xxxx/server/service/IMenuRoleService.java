ce08cc4546677ae622d5b9149851cb699566dfc2
dingc <1109928059@qq.com>
2021-12-24 23:26:37 +0800

    权限组菜单更新功能实现

--- yeb-admin/yeb-server/src/main/java/com/xxxx/server/service/IMenuRoleService.java
+++ yeb-admin/yeb-server/src/main/java/com/xxxx/server/service/IMenuRoleService.java
@@ -2,10 +2,11 @@ package com.xxxx.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xxxx.server.pojo.MenuRole;
+import com.xxxx.server.pojo.RespBean;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author dingc
@@ -13,4 +14,12 @@ import com.xxxx.server.pojo.MenuRole;
  */
 public interface IMenuRoleService extends IService<MenuRole> {
 
+	/**
+	 * 更新角色菜单
+	 *
+	 * @param rid
+	 * @param mids
+	 * @return
+	 */
+	RespBean updateMenuRole(Integer rid, Integer[] mids);
 }
