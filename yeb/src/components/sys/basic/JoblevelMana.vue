2e3c35f9efbcc88bfb39e2c4a29dac013512e9e2
dingc <1109928059@qq.com>
2021-12-26 19:10:07 +0800

    职称添加删除操作

--- yeb/src/components/sys/basic/JoblevelMana.vue
+++ yeb/src/components/sys/basic/JoblevelMana.vue
@@ -15,7 +15,7 @@
 					:value="item">
 				</el-option>
 			</el-select>
-			<el-button size="small" type="primary" icon="el-icon-plus">添加</el-button>
+			<el-button size="small" type="primary" icon="el-icon-plus" @click="addJobLevel">添加</el-button>
 		</div>
 		<div style="margin-top: 10px">
 			<el-table
@@ -48,11 +48,15 @@
 					prop="enabled"
 					label="是否启用"
 					width="150">
+					<template slot-scope="scope">
+						<el-tag v-if="scope.row.enabled" type="success">已启用</el-tag>
+						<el-tag v-else type="danger">未启用</el-tag>
+					</template>
 				</el-table-column>
 				<el-table-column label="操作">
-					<template slot-scope="score">
+					<template slot-scope="scope">
 						<el-button size="small">编辑</el-button>
-						<el-button size="small" type="danger">删除</el-button>
+						<el-button size="small" type="danger" @click="deleteHandler(scope.row)">删除</el-button>
 					</template>
 				</el-table-column>
 			</el-table>
@@ -77,6 +81,53 @@ export default {
 				'初级'
 			],
 			jls: []
+		}
+	},
+	mounted() {
+		this.initJls();
+	},
+	methods: {
+		// 初始化页面, 获取所有职称信息
+		initJls() {
+			this.getRequest('/system/basic/joblevel/').then(resp => {
+				if (resp) {
+					this.jls = resp;
+					this.jl.name = '';
+					this.jl.titleLevel = '';
+				}
+			})
+		},
+		// 添加职称
+		addJobLevel() {
+			if (this.jl.name && this.jl.titleLevel) {
+				this.postRequest('/system/basic/joblevel/', this.jl).then(resp => {
+					if (resp) {
+						this.initJls();
+					}
+				})
+			} else {
+				this.$message.error("字段不能为空哦 ^_^")
+			}
+		},
+		// 删除职称
+		deleteHandler(data){
+			this.$confirm('此操作将永久删除[' + data.name + ']职称, 是否继续?', '提示', {
+				confirmButtonText: '确定',
+				cancelButtonText: '取消',
+				type: 'warning'
+			}).then(() => {
+				this.deleteRequest('/system/basic/joblevel/' + data.id).then(resp => {
+					if (resp) {
+						console.log(resp);
+						this.initJls();
+					}
+				})
+			}).catch(() => {
+				this.$message({
+					type: 'info',
+					message: '已取消删除'
+				});
+			});
 		}
 	}
 }
